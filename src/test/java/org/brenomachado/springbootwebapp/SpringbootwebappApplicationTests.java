5a969fb9790aab29b321095c605dc2f16ffaee65
Breno Batista Machado <brenomachado@gmail.com>
2023-01-28 20:09:47 -0800

    Initial commit

---
+++ src/test/java/org/brenomachado/springbootwebapp/SpringbootwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brenomachado.springbootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
