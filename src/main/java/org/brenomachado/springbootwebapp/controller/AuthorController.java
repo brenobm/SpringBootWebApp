1a50368a438dd24f5e2349091bb93955acc80db9
Breno Batista Machado <brenomachado@gmail.com>
2023-02-01 00:00:43 -0800

    Fix typo

--- src/main/java/org/brenomachado/springbootwebapp/controller/AuthorController.java
+++ src/main/java/org/brenomachado/springbootwebapp/controller/AuthorController.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-spublic class AuthorController {
+@Controller
+public class AuthorController {
     private final AuthorRepository authorRepository;
 
     public AuthorController(AuthorRepository authorRepository) {
